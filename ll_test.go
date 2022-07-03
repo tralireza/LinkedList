b7c89ab520d5a7e9fc5879985dc33ed0527ab7d3
Alireza <26859725+tralireza@users.noreply.github.com>
2022-07-03 14:45:00 +0100

    Linked List 2816m Double a Number Represented as a Linked List -> iterative (optimized: prv pointer)

--- ll_test.go
+++ ll_test.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"fmt"
 	"log"
+	"reflect"
+	"runtime"
 	"strings"
 	"testing"
 )
@@ -63,9 +65,29 @@ func Test2816(t *testing.T) {
 		return Reverse(head)
 	}
 
+	TwoPointer := func(head *ListNode) *ListNode {
+		var prv *ListNode
+		for n := head; n != nil; n = n.Next {
+			n.Val *= 2
+			if n.Val >= 10 {
+				n.Val %= 10
+				if prv != nil {
+					prv.Val++
+				} else {
+					head = &ListNode{1, head}
+				}
+			}
+			prv = n
+		}
+
+		return head
+	}
+
 	type L = ListNode
-	for _, f := range []func(*ListNode) *ListNode{WithReverse, doubleIt} {
-		l := &L{9, &L{9, &L{Val: 9}}}
-		log.Printf("%v -> %v", Draw(l), Draw2(f(l)))
+	for _, f := range []func(*ListNode) *ListNode{TwoPointer, WithReverse, doubleIt} {
+		log.Print("== ", runtime.FuncForPC(reflect.ValueOf(f).Pointer()).Name())
+		for _, l := range []*ListNode{&L{9, &L{9, &L{Val: 9}}}, &L{1, &L{Val: 5}}} {
+			log.Printf("%v -> %v", Draw(l), Draw2(f(l)))
+		}
 	}
 }
